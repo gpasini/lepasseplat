f79a2d2b36450d6eac59ed154d351c12224f7fb6
Grégory Pasini <gpasini@adaptinformatique.fr>
2020-09-24 11:35:47 +0200

    posts

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -18,7 +18,7 @@ class ServicesController extends Controller
     public function __invoke(Request $request)
     {
         return Inertia::render('Services', [
-            'posts' => Post::where('published_at',  '<=', Date::now())->paginate(2)
+            'posts' => Post::where('published_at',  '<=', Date::now())->paginate()
         ]);
     }
 }
