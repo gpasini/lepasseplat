995ac60ffcd1d21574e649102afb3002fa397b13
Grégory Pasini <gpasini@adaptinformatique.fr>
2020-09-24 08:18:45 +0200

    Menu

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Booking;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class BookingController extends Controller
+{
+    public function book(Request $request)
+    {
+
+        Booking::create([
+            'user_id' => Auth::user()->id,
+            'scheduled_meal_id' => $request->get('scheduled_meal_id'),
+            'quantity' => 1
+        ]);
+
+        return Redirect::back();
+    }
+
+    public function unbook(Request $request)
+    {
+        Booking::where('id', $request->get('booking_id'))->delete();
+
+        return Redirect::back();
+    }
+
+    public function setQuantity(Request $request)
+    {
+        if ($request->get('quantity') > 0) {
+            Booking::where('id', $request->get('booking_id'))->update(['quantity' => $request->get('quantity')]);
+        } else {
+            return Redirect::route('unbook', ['booking_id' => $request->get('booking_id')]);
+        }
+
+        return Redirect::back();
+    }
+}
---
+++ resources/js/Components/Meal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="border m-2 p-2" >
+      <img width="100" v-if="meal.photo" :src="`/storage/${meal.photo}`" />
+
+      <div>
+          {{ meal.title }}
+      </div>
+
+      <div>
+          {{ meal.description }}
+      </div>
+      <div>
+          {{ meal.price }} €
+      </div>
+
+      <slot name="actions"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    meal: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
